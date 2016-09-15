7660174a45a58a462cf740604fe2bab1982017ae
Ivo Petkov <ivo@ivopetkov.com>
2016-09-15 12:19:24 +0300

    Added unit tests.

--- tests/Test.php
+++ tests/Test.php
@@ -40,13 +40,93 @@ class Test extends VideoEmbedTestCase
     /**
      * 
      */
-    public function testYouTube()
+    public function testFacebook()
     {
-        $videoEmbed = new IvoPetkov\VideoEmbed('https://www.youtube.com/watch?v=Pwe-pA6TaZk');
-        $this->assertTrue($videoEmbed->url === 'https://www.youtube.com/watch?v=Pwe-pA6TaZk');
-        $this->assertTrue($videoEmbed->title === 'Where the Hell is Matt? 2012');
+        $videoEmbed = new IvoPetkov\VideoEmbed('https://www.facebook.com/SpaceX/videos/10157486010400131/');
+        $this->assertTrue($videoEmbed->url === 'https://www.facebook.com/SpaceX/videos/10157486010400131/');
+        $this->assertTrue($videoEmbed->title === null);
         $videoEmbed->setSize(800, 600);
-        $this->assertTrue($videoEmbed->html === '<iframe width="800" height="600" src="https://www.youtube.com/embed/Pwe-pA6TaZk?feature=oembed" frameborder="0" allowfullscreen></iframe>');
+        $this->assertTrue($videoEmbed->html === '<iframe src="https://www.facebook.com/video/embed?video_id=10157486010400131" width="800" height="600" frameborder="0"></iframe>');
+    }
+
+    /**
+     * 
+     */
+    public function testFlickr()
+    {
+        $videoEmbed = new IvoPetkov\VideoEmbed('https://www.flickr.com/photos/udono/3343551781/');
+        $this->assertTrue($videoEmbed->url === 'https://www.flickr.com/photos/udono/3343551781/');
+        $this->assertTrue($videoEmbed->title === 'P1080929');
+        $videoEmbed->setSize(800, 600);
+        $this->assertTrue($videoEmbed->html === '<a data-flickr-embed="true" href="https://www.flickr.com/photos/udono/3343551781/" title="P1080929 by udono, on Flickr"><img src="https://farm4.staticflickr.com/3600/3343551781_832eb9d5d5_z.jpg?zz=1" width="800" height="600" alt="P1080929"></a><script async src="https://embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>');
+    }
+
+    /**
+     * 
+     */
+    public function testFunnyOrDie()
+    {
+        
+    }
+
+    /**
+     * 
+     */
+    public function testHulu()
+    {
+        $videoEmbed = new IvoPetkov\VideoEmbed('http://www.hulu.com/watch/20807/late-night-with-conan-obrien-wed-may-21-2008');
+        $this->assertTrue($videoEmbed->url === 'http://www.hulu.com/watch/20807/late-night-with-conan-obrien-wed-may-21-2008');
+        $this->assertTrue($videoEmbed->title === 'Wed, May 21, 2008 (Late Night With Conan O\'Brien)');
+        $videoEmbed->setSize(800, 600);
+        $this->assertTrue($videoEmbed->html === '<iframe width="800" height="600" src="http://www.hulu.com/embed.html?eid=0-njKp22bl4GivFXH0lh5w" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowfullscreen> </iframe>');
+    }
+
+    /**
+     * 
+     */
+    public function testKickstarter()
+    {
+        
+    }
+
+    /**
+     * 
+     */
+    public function testNYTimes()
+    {
+        
+    }
+
+    /**
+     * 
+     */
+    public function testOnAol()
+    {
+        
+    }
+
+    /**
+     * 
+     */
+    public function testTed()
+    {
+        
+    }
+
+    /**
+     * 
+     */
+    public function testUstream()
+    {
+        
+    }
+
+    /**
+     * 
+     */
+    public function testViddler()
+    {
+        
     }
 
     /**
@@ -64,13 +144,21 @@ class Test extends VideoEmbedTestCase
     /**
      * 
      */
-    public function testHulu()
+    public function testVine()
     {
-        $videoEmbed = new IvoPetkov\VideoEmbed('http://www.hulu.com/watch/20807/late-night-with-conan-obrien-wed-may-21-2008');
-        $this->assertTrue($videoEmbed->url === 'http://www.hulu.com/watch/20807/late-night-with-conan-obrien-wed-may-21-2008');
-        $this->assertTrue($videoEmbed->title === 'Wed, May 21, 2008 (Late Night With Conan O\'Brien)');
+        
+    }
+
+    /**
+     * 
+     */
+    public function testYouTube()
+    {
+        $videoEmbed = new IvoPetkov\VideoEmbed('https://www.youtube.com/watch?v=Pwe-pA6TaZk');
+        $this->assertTrue($videoEmbed->url === 'https://www.youtube.com/watch?v=Pwe-pA6TaZk');
+        $this->assertTrue($videoEmbed->title === 'Where the Hell is Matt? 2012');
         $videoEmbed->setSize(800, 600);
-        $this->assertTrue($videoEmbed->html === '<iframe width="800" height="600" src="http://www.hulu.com/embed.html?eid=0-njKp22bl4GivFXH0lh5w" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowfullscreen> </iframe>');
+        $this->assertTrue($videoEmbed->html === '<iframe width="800" height="600" src="https://www.youtube.com/embed/Pwe-pA6TaZk?feature=oembed" frameborder="0" allowfullscreen></iframe>');
     }
 
     /**
